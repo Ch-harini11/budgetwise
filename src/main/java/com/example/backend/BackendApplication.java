29e815b7fc5ee8ca96070484a9adf027a834e2d7
C. Harini <chharini2004@gmail.com>
2026-02-14 18:25:12 +0530

    Added Dockerfile

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.example.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,17 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-
-        // Load .env
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")
-                .ignoreIfMissing()
-                .load();
-
-        System.setProperty("MAIL_USER", dotenv.get("MAIL_USER"));
-        System.setProperty("MAIL_PASS", dotenv.get("MAIL_PASS"));
-        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
